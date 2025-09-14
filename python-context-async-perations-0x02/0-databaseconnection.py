e5dd6c6f54801486169d0c4e8a48af4fc8e22eb0
Abdullah Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2025-09-14 18:23:25 +0100

    0-databaseconnection.py

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+class DatabaseConnection(object):
+    def __init__(self, file_name, method):
+        self.file_obj = 
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        try:
+            self.conn = connect_to_prodev
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except Exception as e:
+            print(f"failed to connect to database: {e}")
+
+    def __exit__(self, type, value, traceback):
+        try:
+            if self.conn:
+                if type is None:
+                    self.conn.commit()
+                else:
+                    self.conn.rollback()
+                self.cursor.close()
+                self.conn.close()
+        except Exception as e:
+            print(f"Error closing database connection: {e}")
+
+if __name__ == "__main__":
+    with DatabaseConnection() as cursor:
+        cursor.execute("SELECT * FROM users;")
+        rows = cursor.fetchall()
+        print(f"Query Results: {rows}")
