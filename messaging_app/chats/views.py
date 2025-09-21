263aa98c445dea6b59058ba01cd76eebb80aa2d7
Abdullah Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2025-09-21 23:06:11 +0100

    file update: views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from messaging.models import Conversation, Message
+from messaging.serializers import ConversationSerializer, MessageSerializer, ConversationCreateSerializer, MessageCreateSerializer
+from users.models import CustomUser
+from users.serializers import CustomUserSerializer
+
 
 # Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return ConversationCreateSerializer
+        return ConversationSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(participants_id=self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return MessageCreateSerializer
+        return MessageSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(conversation__participants_id=self.request.user)
